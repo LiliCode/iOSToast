2fc36bb961e4689d05acbc100a591f2fdcfa72c4
LiliCode <lili2011050508@163.com>
2016-06-22 19:27:28 +0800

    first commit

---
+++ Toast/Toast.h
@@ -0,0 +1,16 @@
+//
+//  Toast.h
+//  Toast
+//
+//  Created by 李立 on 16/6/22.
+//  Copyright © 2016年 李立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Toast : UIWindow
+
+
++ (void)makeText:(NSString *)text;
+
+@end
